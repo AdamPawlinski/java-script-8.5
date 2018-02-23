c10cd1495a17a7b641b4f3741179b1d358974d89
Adam Pawliński <adam.pawlinski@gmail.com>
2018-02-23 22:50:38 +0100

    add js script and text

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-
+var a = prompt('enter width');
+var h = prompt('enter height');
+var triangleArea = a * h / 2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert("Triangle area is equal to:" + triangleArea);
